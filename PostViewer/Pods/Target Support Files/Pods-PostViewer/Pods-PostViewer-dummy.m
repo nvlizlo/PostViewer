7ed54a91646bfbaa81c517363900f8c4b1f5cfbb
nvlizlo <>
2018-12-22 21:36:05 +0200

    Added podfile

---
+++ PostViewer/Pods/Target Support Files/Pods-PostViewer/Pods-PostViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PostViewer : NSObject
+@end
+@implementation PodsDummy_Pods_PostViewer
+@end
---
+++ PostViewer/Pods/Target Support Files/Pods-PostViewerTests/Pods-PostViewerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PostViewerTests : NSObject
+@end
+@implementation PodsDummy_Pods_PostViewerTests
+@end
